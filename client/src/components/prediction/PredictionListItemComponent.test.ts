913689903eaf26f9ef763d904c77fa62a1491299
Lee <hello@leejohnmartin.co.uk>
2022-06-11 15:45:57 +0100

    Added: Some kind of indicator for votes

---
+++ client/src/components/prediction/PredictionListItemComponent.test.ts
@@ -0,0 +1,18 @@
+import { stringifyVoteNumber } from "./PredictionListItemComponent"
+
+describe("String Vote Parser", () => {
+  const cases = [
+    [100, "100"],
+    [-100, "100"],
+    [0, "0"],
+    [999, "999"],
+    [1000, "1.0k"],
+    [1034, "1.0k"],
+    [2356, "2.3k"],
+    [-5656, "5.6k"],
+    [12656, "12.6k"]
+  ]
+  test.each(cases)("Returns correct string for each number", (num, str) => {
+    expect(stringifyVoteNumber(num as number)).toBe(str)
+  })
+})
--- client/src/components/prediction/PredictionListItemComponent.tsx
+++ client/src/components/prediction/PredictionListItemComponent.tsx
@@ -1,6 +1,29 @@
 import { Link } from "react-router-dom";
 import { Prediction } from "../../../../types/prediction";
-import { ListItem, ListItemButton, ListItemText } from '@mui/material';
+import { ListItem, ListItemButton, ListItemIcon, ListItemText } from '@mui/material';
+import ArrowDropUpIcon from '@mui/icons-material/ArrowDropUp';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+
+export function stringifyVoteNumber(rawNum: number): string {
+  const num = Math.abs(rawNum);
+  if (num < 1000) {
+    return num.toString();
+  }
+  const thousands = Math.floor(num / 1000)
+  const hundreds = Math.floor((num - thousands * 1000) / 100)
+  return `${thousands}.${hundreds}k`
+}
+
+function VoteIndicator({ sortKey }: { sortKey: string }): JSX.Element {
+  const splitchar = "~"
+  
+  const offset = 5000000
+  const [number] = sortKey.split(splitchar)
+  const parsedVotes = parseInt(number) - offset;
+  return <>
+    {parsedVotes >= 0 ? <ArrowDropUpIcon style={{ color: "green"}}/> : <ArrowDropDownIcon style={{ color: "red"}}/>} {stringifyVoteNumber(parsedVotes)}
+  </>
+}
 
 interface PredictionSummaryProps {
   prediction: Prediction;
@@ -12,6 +35,7 @@ export function PredictionListItemComponent({
   return (
     <ListItem>
       <ListItemButton component={Link} to={`/prediction/${prediction.pageUrl}`}>
+        <ListItemIcon><VoteIndicator sortKey={prediction.sort_key}/></ListItemIcon>
         <ListItemText>{prediction.wiki_title} in {prediction.book_title}</ListItemText>
       </ListItemButton>
     </ListItem>
